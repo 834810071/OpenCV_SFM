940d645ff2357148928cd6c6c3121fbc5630d8fc
834810071 <834810071@qq.com>
2018-12-21 13:56:59 +0800

    Initialization

---
+++ OpenCV_SFM/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/features2d.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
